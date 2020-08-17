42e8c0cd20b6407c5ee074fdd12b28b7cc6f5ae8
mattalagala <matt15y@gmail.com>
2020-08-16 20:54:55 -0500

    updated App.vue and Calculator.vue

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,26 +1,29 @@
-
 <template>
   <div class="calculator">
     <div class="display">{{current || '0'}}</div>
     <div @click="clear" class="btn">C</div>
     <div @click="sign" class="btn">+/-</div>
     <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn operator">÷</div>
+    <div @click="divide" class="btn operator">/</div>
+
     <div @click="append('7')" class="btn">7</div>
     <div @click="append('8')" class="btn">8</div>
     <div @click="append('9')" class="btn">9</div>
-    <div @click="times" class="btn operator">x</div>
+    <div @click="multiply" class="btn operator">x</div>
+
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
     <div @click="minus" class="btn operator">-</div>
+
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div @click="add" class="btn operator">+</div>
+    <div @click="plus" class="btn operator">+</div>
+
     <div @click="append('0')" class="btn zero">0</div>
-    <div @click="dot" class="btn">.</div>
-    <div @click="equal" class="btn operator">=</div>
+    <div @click="decimal" class="btn">.</div>
+    <div @click="equals" class="btn operator">=</div>
   </div>
 </template>
 
@@ -28,33 +31,35 @@
 export default {
   data() {
     return {
+      current: "",
       previous: null,
-      current: '',
       operator: null,
       operatorClicked: false,
-    }
+    };
   },
   methods: {
     clear() {
-      this.current = '';
+      this.current = "";
     },
     sign() {
-      this.current = this.current.charAt(0) === '-' ? 
-        this.current.slice(1) : `-${this.current}`;
+      this.current =
+        this.current.charAt(0) === "-"
+          ? this.current.slice(1)
+          : `-${this.current}`;
     },
     percent() {
       this.current = `${parseFloat(this.current) / 100}`;
     },
     append(number) {
       if (this.operatorClicked) {
-        this.current = '';
-        this.operatorClicked = false;
+        this.current = "";
+        this.operartorClicked = false;
       }
       this.current = `${this.current}${number}`;
     },
-    dot() {
-      if (this.current.indexOf('.') === -1) {
-        this.append('.');
+    decimal() {
+      if (this.current.indexOf(".") === -1) {
+        this.append(".");
       }
     },
     setPrevious() {
@@ -62,67 +67,61 @@ export default {
       this.operatorClicked = true;
     },
     divide() {
-      this.operator = (a, b) => a / b;
-      this.setPrevious();
+      this.operator = (a, b) => {
+        a / b;
+        this.setPrevious();
+      };
     },
-    times() {
-      this.operator = (a, b) => a * b;
-      this.setPrevious();
+    multiply() {
+      this.operator = (a, b) => {
+        a * b;
+        this.setPrevious();
+      };
+    },
+    plus() {
+      this.operator = (a, b) => {
+        a + b;
+        this.setPrevious();
+      };
     },
     minus() {
-      this.operator = (a, b) => a - b;
-      this.setPrevious();
+      this.operator = (a, b) => {
+        a - b;
+        this.setPrevious();
+      };
     },
-    add() {
-      this.operator = (a, b) => a + b;
-      this.setPrevious();
-    },
-    equal() {
-      this.current = `${this.operator(
-        parseFloat(this.current), 
-        parseFloat(this.previous)
-      )}`;
-      this.previous = null;
-    }
-  }
-}
+    equal() {},
+  },
+};
 </script>
 
 <style scoped>
 .calculator {
-  margin: 0 auto;
   width: 400px;
+  margin: 0 auto;
   font-size: 40px;
   display: grid;
   grid-template-columns: repeat(4, 1fr);
   grid-auto-rows: minmax(50px, auto);
 }
+
 .display {
-  grid-column: 1 / 5;
-  background-color: #333;
+  grid-column: 1/5;
+  background-color: black;
   color: white;
 }
+
 .zero {
-  grid-column: 1 / 3;
+  grid-column: 1/3;
 }
+
 .btn {
-  background-color: #F2F2F2;
-  border: 1px solid #999;
+  background-color: #eee;
+  border: 1px solid #333;
 }
+
 .operator {
   background-color: orange;
   color: white;
 }
 </style>
-© 2020 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
