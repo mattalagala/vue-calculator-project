745c7d5ebfea5d974ca983456ea51346e86b874f
mattalagala <matt15y@gmail.com>
2020-08-16 23:11:06 -0500

    added operator functionality

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -14,16 +14,16 @@
     <div @click="append('4')" class="btn">4</div>
     <div @click="append('5')" class="btn">5</div>
     <div @click="append('6')" class="btn">6</div>
-    <div @click="minus" class="btn operator">-</div>
+    <div @click="subtract" class="btn operator">-</div>
 
     <div @click="append('1')" class="btn">1</div>
     <div @click="append('2')" class="btn">2</div>
     <div @click="append('3')" class="btn">3</div>
-    <div @click="plus" class="btn operator">+</div>
+    <div @click="add" class="btn operator">+</div>
 
     <div @click="append('0')" class="btn zero">0</div>
     <div @click="decimal" class="btn">.</div>
-    <div @click="equals" class="btn operator">=</div>
+    <div @click="equal" class="btn operator">=</div>
   </div>
 </template>
 
@@ -31,8 +31,8 @@
 export default {
   data() {
     return {
-      current: "",
       previous: null,
+      current: "",
       operator: null,
       operatorClicked: false,
     };
@@ -67,30 +67,29 @@ export default {
       this.operatorClicked = true;
     },
     divide() {
-      this.operator = (a, b) => {
-        a / b;
-        this.setPrevious();
-      };
+      this.operator = (a, b) => a / b;
+      this.setPrevious();
     },
     multiply() {
-      this.operator = (a, b) => {
-        a * b;
-        this.setPrevious();
-      };
+      this.operator = (a, b) => a * b;
+      this.setPrevious();
     },
-    plus() {
-      this.operator = (a, b) => {
-        a + b;
-        this.setPrevious();
-      };
+    add() {
+      this.operator = (a, b) => a + b;
+      this.setPrevious();
     },
-    minus() {
-      this.operator = (a, b) => {
-        a - b;
-        this.setPrevious();
-      };
+    subtract() {
+      this.operator = (a, b) => a - b;
+      this.setPrevious();
     },
-    equal() {},
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`;
+      this.previous = null;
+      this.operatorClicked = false;
+    },
   },
 };
 </script>
